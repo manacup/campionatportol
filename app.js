c0009dad03f22183fce4a433e251d136a8f7d2c8
manacup <91571517+manacup@users.noreply.github.com>
2023-07-03 12:25:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-var id = Math.floor(Math.random() * Date.now())
-   
-function estadistica(){
-document.getElementById('estadistica').src = "https://docs.google.com/forms/d/e/1FAIpQLScMRS7yABr1mP94vvfs7ye49eL1W7lfwdECvxt_3x58Quv-dQ/formResponse?entry.1842180346=CLICK&entry.1220927547="+id
-}
+
 
  document.addEventListener('DOMContentLoaded', function() {
      M.AutoInit();
@@ -13,21 +9,7 @@ document.getElementById('spinner').style.display='none';
   
 
 };
-//document.getElementById("linkApp").addEventListener("click",estadistica);
-  
-function identificador(){
- 
- var localID = localStorage.getItem("id")
- var nom = localStorage.getItem("jugador")
- var versio = document.getElementById("versio").textContent
- 
- if(localID) {
-     id = localID
- }else{
-     localStorage.setItem("id",id)
- }
- document.getElementById("estadistica").src='https://docs.google.com/forms/d/e/1FAIpQLScMRS7yABr1mP94vvfs7ye49eL1W7lfwdECvxt_3x58Quv-dQ/formResponse?entry.1176666344='+versio+'&entry.1220927547='+id+"&entry.1299550889="+nom
-}
+
   function toggleFullScreen(elem) {
  // ## The below if statement seems to work better ## if ((document.fullScreenElement && document.fullScreenElement !== null) || (document.msfullscreenElement && document.msfullscreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
  if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
@@ -53,9 +35,7 @@ function identificador(){
  }
 }
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
-  }
+
  function canviaUrl(href){
     
    if(document.getElementById("rec")!= undefined){
